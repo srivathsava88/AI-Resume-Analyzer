67e908fa0a9ac61d2c267f5917e3c4da9e108bbc
Srivathsava <srivathsava88@gmail.com>
2026-05-27 12:15:08 +0530

    updated groq model

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def upload_resume():
         """
 
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama-3.1-8b-instant",
             messages=[
                 {
                     "role": "user",
