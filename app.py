580369007615eaf49b5ddf8a0170a3c6651743c4
Srivathsava <srivathsava88@gmail.com>
2026-05-27 11:56:39 +0530

    added pdf upload

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import pdfplumber
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload_resume():
+
+    if "resume" not in request.files:
+        return "No file uploaded"
+
+    file = request.files["resume"]
+
+    if file.filename == "":
+        return "No selected file"
+
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    file.save(filepath)
+
+    text = ""
+
+    with pdfplumber.open(filepath) as pdf:
+        for page in pdf.pages:
+            extracted = page.extract_text()
+            if extracted:
+                text += extracted
+
+    return f"""
+    <h2>Resume Uploaded Successfully</h2>
+    <pre>{text[:3000]}</pre>
+    """
