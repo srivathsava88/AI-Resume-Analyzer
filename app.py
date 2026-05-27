5b8966624900cffe6b5b5eeaed612bf523681768
Srivathsava <srivathsava88@gmail.com>
2026-05-27 11:52:03 +0530

    added html frontend

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "ResumeIQ Backend Running on Vercel"
+    return render_template("index.html")
